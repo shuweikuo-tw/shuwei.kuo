0cbff85cb809b48cde78007cc3f878f9721ef98b
twshutech <shuwei_kuo@outlook.com>
2021-11-07 00:06:19 +0800

    js

--- assets/js/theme-control.js
+++ assets/js/theme-control.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var date = new Date();
+  console.log('its ',date.getHours(),' oclock');
   if(date.getHours()>=18 || date.getHours()<6){
     document.documentElement.setAttribute("nighteye", "active");
   }
